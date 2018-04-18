0f2c9b63327852d8609689e9c18920e817050253
Antoine Rey <antoinerey38@gmail.com>
2018-04-18 15:19:14 +0200

    chore(bundler): Switch back to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   devtool: 'source-map',
   entry: path.resolve(process.cwd(), 'src/index.js'),
   output: {
